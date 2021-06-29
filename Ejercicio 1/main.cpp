e65d755cea967882754feda2895b3ad2d30c82a3
jneirar <jorge.neira@utec.edu.pe>
2021-06-28 22:48:41 -0500

    Modify Ejercicio 1

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,40 +1,5 @@
-#include "../lib/lib.h"
-
-string shortestString(vector<vector<ll>> transition, ll n){
-    setType sInit;
-    for(ll i=0; i<n; i++)  sInit.insert(i);                             //O(nlg(n))
-
-    //BFS
-    map<setType, string> shortString;                                   //max: 2^n
-    set<setType> visited;                                               //max: 2^n
-    queue<setType> qState;                                             
-    qState.push(sInit);                                                 //O(1)
-    visited.insert(sInit);                                              //O(1)
-    shortString[sInit] = "";                                            //O(1)
-    while(!qState.empty()){                                             //O(V + E) | V*(costo de hacer la transición)
-        setType sNext0, sNext1;
-        for(ll state : qState.front()){                                 //O(n)
-            sNext0.insert(transition[state][0]);                        //O(lg(n))
-            sNext1.insert(transition[state][1]);                        //O(lg(n))
-        }
-        if(!visited.count(sNext0)){                                     //O(lg(n))
-            visited.insert(sNext0);                                     //O(lg(2^n)) = O(n)
-            shortString[sNext0] = shortString[qState.front()] + '0';    //O(1 + lg(2^n)) = O(n)
-            qState.push(sNext0);                                        //O(1)
-        }
-        if(!visited.count(sNext1)){                                     
-            visited.insert(sNext1);                                     
-            shortString[sNext1] = shortString[qState.front()] + '1';    
-            qState.push(sNext1);                                        
-        }
-        qState.pop();                                                   //O(1)
-        if(sNext0.size() == 1)                                          //O(1)
-            return shortString[sNext0];                                 //O(1)
-        if(sNext1.size() == 1)
-            return shortString[sNext1]; 
-    }
-    return ".";
-}
+#include "../lib.h"
+#include "../functions.h"
 
 int main(){
     ll n, qi, nqf; 
