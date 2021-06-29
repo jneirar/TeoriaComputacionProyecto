b10fffd10b5dc71de3b2d81e7fc88966f5613cae
jneirar <jorge.neira@utec.edu.pe>
2021-06-28 22:49:17 -0500

    Add Ejercicio 3

---
+++ Ejercicio 3/main.cpp
@@ -0,0 +1,27 @@
+#include "../lib.h"
+#include "../functions.h"
+
+int main(){
+    ll n, qi, nqf; 
+    cin >> n >> qi >> nqf;
+    unordered_set<ll> qf;
+    ll tmp1, tmp2;
+    for(ll i=0; i<nqf; i++){
+        cin >> tmp1;
+        qf.insert(tmp1);
+    }
+    vector<vector<ll>> transition(n);
+    for(ll i=0; i<n; i++)   transition[i].resize(2);
+    for(ll i=0; i<2*n; i++)
+        cin >> tmp1 >> tmp2 >> transition[tmp1][tmp2];
+
+    if(!Dec_Sinc(n, transition)){
+        cout << "NO\n";
+        return 0;
+    }   
+    string cad = Cad_Sinc(n, transition);
+    cout << "Cadena: " << cad << "\n";
+    cout << "Tamanio: " << cad.length() << "\n";
+    
+    return 0;
+}
